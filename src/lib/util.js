382efda4808ffd663ae7dec47b47b1f2da7e6242
Script Raccoon <scriptraccoon@gmail.com>
2023-07-09 22:08:03 +0200

    clear

--- src/lib/util.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(dateString) {
-    const date = new Date(dateString);
-    const d = date.getDate().toString().padStart(2, "0");
-    const m = (date.getMonth() + 1).toString().padStart(2, "0");
-    const y = date.getFullYear().toString();
-    return `${d}.${m}.${y}`;
-}
